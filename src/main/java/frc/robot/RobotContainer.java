630913f61dd7ed7a9e9023442a8cf888496b4072
4512 Otter Chaos <otterchaos4512@gmail.com>
2020-02-01 13:56:00 -0800

    finally finished path restructure
    -reversing now actually works

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -59,7 +59,7 @@ public class RobotContainer {
         configureButtonBindings(); // Attach functionality to controller
 
         commandChooser.setDefaultOption("Nothing", new InstantCommand(() -> drivetrain.tankDrive(0,0), drivetrain));
-        /*
+        
         commandChooser.addOption("Forward", 
             new BasicRamseteTest(drivetrain, paths.forward)
         );
@@ -68,7 +68,6 @@ public class RobotContainer {
 
             .andThen(new BasicRamseteTest(drivetrain, paths.forward.getReversed()))
         );
-        */
         
         commandChooser.addOption("Example", 
             new BasicRamseteTest(drivetrain, paths.example)
@@ -78,41 +77,6 @@ public class RobotContainer {
             .andThen(new BasicRamseteTest(drivetrain, paths.example.getReversed()))
         );
         
-        TrajectoryConfig testConfig = OCPath.getDefaultConfig(drivetrain);
-        System.out.println("Normal Forward");
-        for(State state:TrajectoryGenerator.generateTrajectory(Poses.example, testConfig).getStates()){
-            System.out.println(state.toString());
-        }
-        System.out.println("----------");
-        System.out.println("Config reversed: "+testConfig.isReversed());
-
-        System.out.println("Normal Backward");
-        for(State state:TrajectoryGenerator.generateTrajectory(OCPath.getReversedPoses(Poses.example), 
-            testConfig.setReversed(true)).getStates()){
-                System.out.println(state.toString());
-        }
-        System.out.println("----------");
-        System.out.println("Config reversed: "+testConfig.isReversed());
-
-        /*
-        System.out.println("OCPath Forward");
-        for(State state:paths.example.getStates()){
-            System.out.println(state.toString());
-        }
-        System.out.println("----------");
-        System.out.println("Config reversed: "+paths.example.getConfig().isReversed());
-        */
-
-        //OCPath reverseExample = paths.example.getReversed();
-        /*
-        System.out.println("OCPath Backward");
-        for(State state:reverseExample.getStates()){
-            System.out.println(state.toString());
-        }
-        System.out.println("----------");
-        System.out.println("Config reversed: "+reverseExample.getConfig().isReversed());
-        */
-
         SmartDashboard.putData("Auto mode", commandChooser);
     }
 
--- src/main/java/frc/robot/common/OCPath.java
+++ src/main/java/frc/robot/common/OCPath.java
@@ -10,14 +10,12 @@ package frc.robot.common;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static frc.robot.common.Constants.*;
 
-import frc.robot.common.Paths.Poses;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
-import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
@@ -25,64 +23,95 @@ import edu.wpi.first.wpilibj.trajectory.constraint.CentripetalAccelerationConstr
 import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
 
 /**
- * 
+ * Extension of the {@link Trajectory} class that simplifies the construction and use of Trajectories.
+ * Trajectories are constructed by default with a preset configuration.
+ * To retrieve a trajectory in reverse, simply use .getReversed()
  */
 public class OCPath extends Trajectory{
-    private TrajectoryConfig config;
+    private TrajectoryConfig config; // Store config for reversing
 
+    /**
+     * Constructs a new Trajectory using {@link TrajectoryGenerator} and quintic hermite splines.
+     * Drivetrain is given to use a default config with its kinematics.
+     */
     public OCPath(List<Pose2d> poses, Drivetrain drive){
         this(TrajectoryGenerator.generateTrajectory(poses, getDefaultConfig(drive)).getStates(), getDefaultConfig(drive));
     }
+    /**
+     * Constructs a new Trajectory using {@link TrajectoryGenerator} and clamped cubic splines,
+     * where the heading at the interior waypoints is automatically determined.
+     * Drivetrain is given to use a default config with its kinematics.
+     */
+    public OCPath(Pose2d start, List<Translation2d> interiorWaypoints, Pose2d end, Drivetrain drive){
+        this(TrajectoryGenerator.generateTrajectory(start, interiorWaypoints, end, getDefaultConfig(drive)).getStates(), 
+            getDefaultConfig(drive));
+    }
+    /**
+     * Constructs a new Trajectory with given states and configuration. This constructor should be
+     * used when more specific control is required, either with a specific trajectory implementation or
+     * with a manually defined configuration.
+     */
     public OCPath(List<State> states, TrajectoryConfig config){
-        super(states);
-        
-        System.out.println("--States---");
-        for(State state:states){
-            System.out.println(state.toString());
-        }
-        System.out.println("----------");
-        System.out.println("Config reversed: "+(config.isReversed()));
-        
+        super(states);        
         this.config = config;
     }
-    /*
-    public OCPath(List<State> states, TrajectoryConfig config){
-        super(states);
-    }
-    */
 
+    /**
+     * Returns a clone of the given {@link TrajectoryConfig}.
+     */
+    public static TrajectoryConfig cloneConfig(TrajectoryConfig config){
+        return new TrajectoryConfig(config.getMaxVelocity(), config.getMaxAcceleration())
+        .addConstraints(config.getConstraints())
+        .setReversed(config.isReversed());
+    }
+
+    /**
+     * Returns the default configuration to be used for generating trajectories.
+     * @param drive Drivetrain to specify kinematics
+     */
     public static TrajectoryConfig getDefaultConfig(Drivetrain drive){
         return new TrajectoryConfig(kMaxMetersLowGear-1, kMaxAccelerationMeters-0.3)
             .setKinematics(drive.getKinematics())
             .addConstraint(new CentripetalAccelerationConstraint(kMaxCentripetalAccelerationMeters)) // Take corners slow
             .addConstraint(new DifferentialDriveVoltageConstraint(drive.getFeedForward(), drive.getKinematics(), 10)); // Account for voltage sag
     }
-    /*
-    public List<Pose2d> getPoses(){
-        List<Pose2d> poses = this.getStates().stream().map(state -> state.poseMeters).collect(Collectors.toList());
-        return poses;
-    }*/
+    /**
+     * Returns a cloned list of Trajectory states.
+     */
     public List<State> getStates(){
         return new ArrayList<State>(super.getStates());
     }
+    /**
+     * Returns a clone of this Trajectory's configuration.
+     */
     public TrajectoryConfig getConfig(){
-        return new TrajectoryConfig(config.getMaxVelocity(), config.getMaxAcceleration())
-            .addConstraints(config.getConstraints())
-            .setReversed(config.isReversed());
+        return cloneConfig(config);
     }
 
+    /**
+     * Returns a clone of this OCPath, reversing the trajectory to
+     * be followed backward.
+     * Ex: Follow a trajectory, and then follow the same trajectory reversed
+     * to return to the starting point.
+     */
     public OCPath getReversed(){
-        System.out.println("! Got Reversed !");
         return new OCPath(getReversedStates(getStates()), 
             getReversedConfig(getConfig()));
     }
+    /**
+     * Returns a cloned, reversed list of the given poses.
+     */
     public static List<Pose2d> getReversedPoses(List<Pose2d> poses){
-        List<Pose2d> reversedPoses = poses.stream().collect(Collectors.toList());
+        List<Pose2d> reversedPoses = new ArrayList<Pose2d>(poses);
         Collections.reverse(reversedPoses);
         return reversedPoses;
     }
+    /**
+     * Returns a cloned, reversed list of the given states.
+     * Each state is reconstructed with negative velocity and
+     * the correct timepoint for following backwards.
+     */
     public static List<State> getReversedStates(List<State> states){
-        System.out.println("! Got Reversed States !");
         List<State> reversedStates = new ArrayList<State>(states);
         Collections.reverse(reversedStates);
         
@@ -97,9 +126,11 @@ public class OCPath extends Trajectory{
         }
         return reversedStates;
     }
+    /**
+     * Returns a clone of the given {@link TrajectoryConfig} with the reverse flag set to true.
+     */
     public static TrajectoryConfig getReversedConfig(TrajectoryConfig config){
-        return new TrajectoryConfig(config.getMaxVelocity(), config.getMaxAcceleration())
-            .addConstraints(config.getConstraints())
+        return cloneConfig(config)
             .setReversed(!config.isReversed());
     }
 
